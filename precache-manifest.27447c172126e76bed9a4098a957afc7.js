ff1ef393a38ad257260a7f8d145000a6b230f7c3
felixcalo <felixcalo12@hotmail.com>
2020-08-13 18:12:19 +0100

    Updates

---
+++ precache-manifest.27447c172126e76bed9a4098a957afc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "085f078c77f9f539c133e90f59f22a21",
+    "url": "/AnacondaFlix/web/index.html"
+  },
+  {
+    "revision": "4cabe08025cc5960e89c",
+    "url": "/AnacondaFlix/web/static/css/main.b1248acd.chunk.css"
+  },
+  {
+    "revision": "33075db5f8fafa6d03e7",
+    "url": "/AnacondaFlix/web/static/js/2.bb09a50f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/AnacondaFlix/web/static/js/2.bb09a50f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cabe08025cc5960e89c",
+    "url": "/AnacondaFlix/web/static/js/main.ccfee163.chunk.js"
+  },
+  {
+    "revision": "117cc0992782acda4135",
+    "url": "/AnacondaFlix/web/static/js/runtime-main.b9bb86cb.js"
+  }
+]);
